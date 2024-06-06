08a89700c8ff3effea87a9fcb30fe2c2b597e78e
John McEwan <bonzojo@proton.me>
2024-06-06 20:01:40 +0100

    reactionOne

---
+++ src/Footer.jsx
@@ -0,0 +1,13 @@
+function Footer(){
+    return(
+    <footer>
+        <p>The high hats dance, like a feather. I'm in...I'm in a paralytic loveless dream.
+            Not a single face I've seen, is a friend. I recognise. Or recognises me.
+            I black out. I don't want your dim sum. I wanna dance til the sun comes.
+            I wanna fight your cousin. I wanna tear through the night like a angel. Flare.
+            I'm a ten foot snare. That cuts through the sky with no care. no care. no.
+        </p>
+    </footer>
+    );
+}
+export default Footer
